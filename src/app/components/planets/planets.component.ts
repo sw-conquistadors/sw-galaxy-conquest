4a38b7ee500055fe9da915cd4edc4955bb4db92b
Kenny Phu <kphu@outlook.com>
2022-01-21 20:11:54 -0500

    create components, models, services

---
+++ src/app/components/planets/planets.component.ts
@@ -0,0 +1,30 @@
+import { PlanetService } from './../../services/planet.service';
+import { Planet } from './../../models/planet';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planets',
+  templateUrl: './planets.component.html',
+  styleUrls: ['./planets.component.css']
+})
+export class PlanetsComponent implements OnInit {
+
+  title = "Explore the Planets"
+
+  public planets: Planet[] = [];
+
+
+  constructor(private pServ: PlanetService) { }
+
+  ngOnInit(): void {
+    this.findAllPlanets();
+    console.log(this.planets)
+  }
+
+  findAllPlanets() {
+    // We are calling a method from the user service that will return an Observable
+    this.planets = this.pServ.findAllPlanets();
+  }
+
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,57 @@
+import { localUrl } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
+import { User } from '../models/user';
+import { catchError, Observable, throwError } from 'rxjs';
+import { awsUrl } from 'src/environments/environment'
+
+const url = `${awsUrl}/users`
+const lUrl = `${localUrl}/users`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  // get all
+  findAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(url)
+            .pipe(catchError(this.handleError));
+  }
+
+  registerUser(user: User): Observable<User> {
+    return this.http.post<User>(`${lUrl}/add`, user, this.httpOptions)
+          .pipe(catchError(this.handleError));
+  }
+
+  // get 1
+  findUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${url}/${id}`)
+            .pipe(catchError(this.handleError));
+  }
+
+  // delete
+  removeUserById(id: number) {
+    return this.http.delete<User>(`${url}/${id}`)
+            .pipe(catchError(this.handleError));
+  }
+
+  private handleError(httpError: HttpErrorResponse) {
+    if (httpError.error instanceof ErrorEvent) {
+      console.log('An error occurred: ', httpError.error.message)
+    } else {
+      console.error(`
+        Backend returned code ${httpError.status},
+        body was: ${httpError.error}
+      `)
+    }
+    return throwError(() => new Error('Something really bad happened, please try again later'))
+  }
+
+}
+
