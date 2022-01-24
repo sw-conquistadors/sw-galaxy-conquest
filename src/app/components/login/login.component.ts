0bfc8959ac8e36339580e757a76ceafe152e69e2
Jesse Rodriguez <robotsarecrazy64@gmail.com>
2022-01-24 09:55:59 -0700

    Finish Log In Method

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,31 +2,46 @@ import { ClientMessage } from './../../models/client-message';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
-//import { NavComponent.authenticated } from 'src/app/components/nav/nav.component';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public authenticated: boolean = false;
   title= "Log In";
-  public user = new User('','')
+  public user = new User(0,'','')
   clientMessage: ClientMessage = new ClientMessage('');
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, public authService:AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
-  public validateUser(username: string, password: string): void {
-    this.user.username = username;
-    this.user.password = password;
+  public validateUser(): void {
     if(this.userService.validateUser(this.user)
       .subscribe(
-        data => this.clientMessage.message = `Hello ${data.username}`, // data => this.userService.logger.log(data),
+        data => {
+          this.user = data;
+          this.clientMessage.message = `Hello ${data.username}`;
+       },
         error => this.clientMessage.message = `Unable to Login with that username and password`   // error => this.userService.logger.error(error)
       )){
-        //authenticated = true;
+        this.authService.authenticated = true;
       }
+  }
+
+  public updateUser(): void {
+    if(this.userService.registerUser(this.user)
+    .subscribe(
+      data => {
+        this.user = data;
+        this.clientMessage.message = `Successfully updated ${data.username}`;
+     }, // data => this.userService.logger.log(data),
+      error => this.clientMessage.message = `Something went wrong. ${error}`   // error => this.userService.logger.error(error)
+    )){
+      this.authService.authenticated = true;
+    }
   }
 
 }
